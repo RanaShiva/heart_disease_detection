bfd23d55b802d0a73f36a4a8884bb63561d4f428
shiva-rana <shivam4learning@gmail.com>
2021-05-22 16:01:44 +0530

    Initial commit

---
+++ heart.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import warnings
+import pickle
+from sklearn.metrics import accuracy_score
+warnings.filterwarnings('ignore')
+
+from sklearn.neighbors import KNeighborsClassifier
+df = pd.read_csv('/home/shiva-rana/ml_in_healthcare/heart.csv')
+
+from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
+# standardScaler = StandardScaler()9
+# columns_to_scale = ['age', 'trestbps','chol','thalach','oldpeak']
+# df[columns_to_scale] = standardScaler.fit_transform(df[columns_to_scale])
+
+cols=['age', 'trestbps','chol','thalach','oldpeak']
+y=df.target
+X=df[cols]
+print(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+knn_classifier = KNeighborsClassifier(n_neighbors=12)
+
+knn_classifier.fit(X,y)
+y_pred = knn_classifier.predict(X_test)
+print('Accuracy : {}'.format(accuracy_score(y_test, y_pred)))
+
+pickle.dump(knn_classifier,open('model.pkl','wb'))
